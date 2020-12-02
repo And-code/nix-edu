d0bdfcf7bc8a0bcb373b5b40a1481ead79e827c2
And-code <andrewgr.mob@gmail.com>
2020-12-02 16:58:47 +0200

    task 3. Simple site structure.

--- index.php
+++ index.php
@@ -1,2 +1,48 @@
-<?php
-echo "Hello, NIX Education";
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>NIX курс PHP. Практика</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<header>
+    <h1>NIX курс PHP. Практика</h1>
+
+    <nav>
+        <ul>
+            <li><span>Главная</span></li>
+            <li><a href="#">Урок 1</a></li>
+            <li><a href="#">Урок 2</a></li>
+            <li><a href="#">Урок 3</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+
+    <article>
+        <h2>Добро пожаловать!</h2>
+
+        <p>На этом сайте располагаются результаты выполнения практического задания для курса NIX по PHP.</p>
+
+        <p>Первые три урока (урок 1, 2, 3) относятся к части "Beginner. Практика".</p>
+    </article>
+
+</main>
+
+<footer>
+    <p>NIX курс PHP. Практика.</p>
+
+    <p><a href="https://education.nixsolutions.com/">NIX education</a></p>
+</footer>
+
+
+</body>
+</html>
