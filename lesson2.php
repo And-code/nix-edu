c217b6cb5491e65b226b6637beeb3a12fdf0046c
And-code <andrewgr.mob@gmail.com>
2020-12-03 12:50:48 +0200

    task 5. lesson 2. Colored times table.

--- lesson2.php
+++ lesson2.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <title>Урок 1. NIX курс PHP. Практика</title>
+    <title>Урок 2. NIX курс PHP. Практика</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
     <!--[if lt IE 9]>
@@ -13,13 +13,13 @@
 <body>
 
 <header>
-    <h1>Урок 1</h1>
+    <h1>Урок 2</h1>
 
     <nav>
         <ul>
             <li><a href="index.php">Главная</a></li>
-            <li><span>Урок 1</span> </li>
-            <li><a href="lesson2.php">Урок 2</a></li>
+            <li><a href="lesson1.php">Урок 1</a></li>
+            <li><span>Урок 2</span> </li>
             <li><a href="#">Урок 3</a></li>
         </ul>
     </nav>
@@ -28,7 +28,14 @@
 <main>
 
     <article>
-        <h2>Таблица умножения</h2>
+        <h2>Таблица умножения разноцветная</h2>
+
+        <p>
+            <span class="number-1">1</span> - красный, <br>
+            <span class="number-2">2</span> - зеленый, <br>
+            <span class="number-3">3</span> - желтый, <br>
+            <span class="number-4">4</span> - синий <br>
+        </p>
 
         <table>
 
@@ -43,10 +50,20 @@
 
                         for($x=$limit; $x<($limit+$step); $x++) {
                             echo "<td>";
+                            $str = "";
                             for ($y = 1; $y < 11; $y++) {
-                                echo "$x&times;$y=" . $x * $y . "<br>";
+                                $str .= "$x&times;$y=" . $x * $y . "<br>";
                             }
-                            echo "</td>";
+                            $str = str_replace(
+                                ['1', '2', '3', '4'],
+                                [
+                                    '<span class="number-1">1</span>',
+                                    '<span class="number-2">2</span>',
+                                    '<span class="number-3">3</span>',
+                                    '<span class="number-4">4</span>',
+                                ],
+                                $str);
+                            echo $str . "</td>";
                         }
                     ?>
                 </tr>
